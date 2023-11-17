2faa6e20d09830282353929e5acdf812dcd46349
ShijazKs <hafizshijaz@gmail.com>
2023-11-17 21:52:31 +0530

    input-disable added

--- client/realtime/src/app/Screen.js
+++ client/realtime/src/app/Screen.js
@@ -3,8 +3,8 @@ import React from "react";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
-//const socket = io.connect("http://localhost:3001");
-const socket = io.connect("https://nexus-chat.glitch.me/");
+const socket = io.connect("http://localhost:3001");
+//const socket = io.connect("https://nexus-chat.glitch.me/");
 
 const Screen = () => {
   const [room, setRoom] = useState(""); // input room
@@ -13,9 +13,9 @@ const Screen = () => {
   const [username, setUsername] = useState(""); //username
   const [myroom, setMyroom] = useState(""); // current room
 
-  const sendDataToServer = (data) => {
-    socket.emit("data_from_client", data);
-  };
+  // const sendDataToServer = (data) => {
+  //   socket.emit("data_from_client", data);
+  // };
 
   // Filter out empty messages from messagesList
   const filteredMessagesList = messagesList.filter(
@@ -130,26 +130,6 @@ const Screen = () => {
       >
         <ul>
           {filteredMessagesList.map((messageItem, index) => (
-            // <li key={index}>
-            //   {messageItem.senderSocketId === "me" ? (
-            //     <div
-            //       id="right"
-            //       className="bg-green-300 max-w-min min-w-fit p-3 mb-4 rounded-lg ml-auto"
-            //     >
-            //       <p>{messageItem.message}</p>
-            //     </div>
-            //   ) : (
-            //     // Check if message is not empty before rendering
-            //     messageItem.message !== "" && (
-            //       <div className="bg-white max-w-min min-w-fit p-3 mb-4 rounded-lg">
-            //         <h3 className="font-semibold">
-            //           {messageItem.senderSocketId}
-            //         </h3>
-            //         <p>{messageItem.message}</p>
-            //       </div>
-            //     )
-            //   )}
-            // </li>
             <li key={index}>
               {messageItem.senderSocketId === "me" ? (
                 <div
@@ -187,7 +167,7 @@ const Screen = () => {
       {/* message input */}
       <div
         id="msg"
-        className="w-5/6 form-outline overflow-hidden border border-green-500 h-14 mt-10 flex rounded-lg"
+        className="bg-gray-100 w-5/6 form-outline overflow-hidden border border-green-500 h-14 mt-10 flex rounded-lg"
       >
         <textarea
           id="msgbox"
@@ -195,6 +175,7 @@ const Screen = () => {
           onChange={(e) => {
             setMessage(e.target.value);
           }}
+          disabled={!username}
           onKeyDown={(e) => {
             if (window.innerWidth > 768) {
               if (e.key === "Enter" && !e.shiftKey) {
@@ -204,7 +185,7 @@ const Screen = () => {
               }
             }
           }}
-          className="form-control w-5/6 p-3 resize-none text-xl focus:border-bg-white rounded-l-lg"
+          className="whitespace-pre-wrap form-control w-5/6 p-3 resize-none text-xl focus:border-bg-white rounded-l-lg"
           rows="3"
         ></textarea>
         <button
@@ -227,6 +208,24 @@ const Screen = () => {
           </svg>
         </button>
       </div>
+      <footer className="lg:fixed md:static fixed w-full px-2 bottom-0 md:mb-2 ">
+        <div className="foot">
+        <a href="https://shijazks.vercel.app"><h4 className="font-serif text-md italic text-indigo-900 pl-2">jazDesign</h4></a>
+        <a href="https://github.com/ShijazKS/OpenTalk" className="svg-link">
+          <svg
+            className="svg-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="30"
+            height="30"
+            viewBox="0 0 64 64"
+          >
+            <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"></path>
+          </svg>
+        </a>
+        </div>
+      </footer>
     </>
   );
 };
