e4fdf7a0be6de3212b9545c9f04154b2731bbed4
mashhoor-ahdal <mashhoorahdal2@gmail.com>
2024-02-17 23:40:48 +0530

    Copy button added

--- client/realtime/src/app/components/ChatScreen.js
+++ client/realtime/src/app/components/ChatScreen.js
@@ -1,6 +1,10 @@
 import React from "react";
+import CopyButton from "./CopyButton";
 
 const ChatScreen = ({ filteredMessagesList }) => {
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+  };
   return (
     <div
       id="screen"
@@ -22,8 +26,12 @@ const ChatScreen = ({ filteredMessagesList }) => {
             ) : (
               <div
   id="left"
-  className="bg-white dark:bg-fuchsia-800 mr-3 md:mr-10 lg:mr-24 max-w-fit p-3 mb-4 mr-auto rounded-lg break-all whitespace-pre-wrap shadow-md dark:shadow-xl relative"
+  className="bg-white dark:bg-fuchsia-800  md:mr-10 lg:mr-24 max-w-fit p-3 mb-4 mr-auto rounded-lg break-all whitespace-pre-wrap shadow-md dark:shadow-xl relative"
 >
+<CopyButton
+                  className="top-0 left-0 "
+                  onClick={() => copyToClipboard(messageItem.message)}
+/>
   <h3 className="text-teal-600 dark:text-yellow-200 font-semibold text-sm">
     {messageItem.senderSocketId}
   </h3>
