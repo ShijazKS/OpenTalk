7c9ddf3ad1fc8f1da33eb0f49636dad77ba272b0
ShijazKs <hafizshijaz@gmail.com>
2024-03-10 08:08:34 +0530

    meta data updated

--- client/realtime/src/app/layout.js
+++ client/realtime/src/app/layout.js
@@ -4,16 +4,17 @@ import Head from "next/head";
 
 
 
-export const metadata = {
-  title: 'OpenTalk',
-  description: 'Created by jazDesign',
-}
+// export const metadata = {
+//   title: 'OpenTalk',
+//   description: 'Created by jazDesign',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk" />
+      <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk,open talk" />
+      <meta name="description" content="Realtime room-based authentless chat application" />
       </Head>
       <body>{children}</body>
     </html>
