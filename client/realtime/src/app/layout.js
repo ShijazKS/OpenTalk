1bfe3fc0fcb87fe5c8fccf641e1ef14a234a3816
sameemul-haque <samhaqk@gmail.com>
2024-05-07 20:59:53 +0530

    update metadata

--- client/realtime/src/app/layout.js
+++ client/realtime/src/app/layout.js
@@ -4,29 +4,31 @@ import Head from "next/head";
 
 
 
-// export const metadata = {
-//   title: 'OpenTalk',
-//   description: 'Created by jazDesign',
-// }
+export const metadata = {
+  title: 'OpenTalk',
+  description: 'Realtime room-based authentless chat application',
+  keywords: ["opentalk", "jazdesign", "shijazks", "open-talk", "open talk"],
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Shijaz KS" },
+    {
+      name: "Shijaz KS",
+      url: "https://github.com/shijazks/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      {/* <Head>
         <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk,open talk" />
         <meta name="description" content="Realtime room-based authentless chat application" />
-        <meta name="application-name" content="OpenTalk" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="OpenTalk" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
+      </Head> */}
       <title>OpenTalk</title>
       <body>{children}</body>
     </html>
