fd70156eed7a45d802fc87920e70279d35398bfc
sameemul-haque <samhaqk@gmail.com>
2024-05-07 20:44:05 +0530

    feat: add pwa support

--- client/realtime/src/app/layout.js
+++ client/realtime/src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 import Head from "next/head";
 
 
@@ -13,8 +13,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk,open talk" />
-      <meta name="description" content="Realtime room-based authentless chat application" />
+        <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk,open talk" />
+        <meta name="description" content="Realtime room-based authentless chat application" />
+        <meta name="application-name" content="OpenTalk" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="OpenTalk" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <title>OpenTalk</title>
       <body>{children}</body>
