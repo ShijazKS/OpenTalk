73e15ad470cce9357f85c15c0f1265401089d35e
ShijazKs <hafizshijaz@gmail.com>
2024-05-09 18:25:48 +0530

    logo setup

--- client/realtime/src/app/layout.js
+++ client/realtime/src/app/layout.js
@@ -1,36 +1,34 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import Head from "next/head";
 
-
-
 export const metadata = {
-  title: 'OpenTalk',
-  description: 'Realtime room-based authentless chat application',
+  title: "OpenTalk",
+  description: "Realtime room-based authentless chat application",
   keywords: ["opentalk", "jazdesign", "shijazks", "open-talk", "open talk"],
   generator: "Next.js",
   manifest: "/manifest.json",
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
   authors: [
-    { name: "Shijaz KS" },
     {
       name: "Shijaz KS",
       url: "https://github.com/shijazks/",
     },
+    { name: "Sameemul Haque", url: "https://github.com/sameemul-haque/" },
   ],
   viewport:
     "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <meta name="keywords" content="opentalk,jazdesign,shijazks,open-talk,open talk" />
-        <meta name="description" content="Realtime room-based authentless chat application" />
-      </Head> */}
-      {/* <title>OpenTalk</title> */}
+      <link
+        rel="icon"
+        href="/icons/chat.svg"
+        type="image/svg+xml"
+      />
       <body>{children}</body>
     </html>
-  )
+  );
 }
