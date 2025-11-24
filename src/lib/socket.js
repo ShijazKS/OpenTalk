389c46712b75bdeec5a51d5d5af96019bc739805
shijaz-ks <70143317+ShijazKS@users.noreply.github.com>
2025-11-24 22:42:54 +0530

    upd socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -4,7 +4,8 @@ let socket = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io("http://localhost:5000");
+    // socket = io("http://localhost:5000");
+    socket = io("https://opentalk-server.onrender.com");
   }
   return socket;
 }
